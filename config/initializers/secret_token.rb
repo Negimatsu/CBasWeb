553035b9649a8b15a15e5a6c298162e19f7228ae
Negimatsu <ong.dkrab@gmail.com>
2013-07-01 13:26:02 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CBasWebRVM::Application.config.secret_token = '3ac98f0660f59bdb9337e20cbc88a37518b57da4794346d3a37c38c182c425f8d4822c5c4b4b8d1f2a498987069cdc9ff63b0e53ecf9435740f4c60b50a4a149'
